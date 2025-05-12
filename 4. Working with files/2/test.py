1e394968086d9aee17f66662192cdb673cb1592c
VladNkov <vladnkov88@gmail.com>
2025-05-12 18:31:01 +0200

    added new tasks

--- 4. Working with files/2/test.py
+++ 4. Working with files/2/test.py
@@ -1,7 +1,7 @@
-import csv
+import json
 
-with open('products.csv', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    for line in reader:
-        print(*line.values())
-        break
+weekdays = {i: day for i, day in enumerate(['Sunday', 'Monday', 'Tuesday'])}
+
+weekdays_json = json.dumps(weekdays, separators=('; ', '-'))
+
+print(weekdays_json)
