f1c7f682837f941194d9c6b292c818b264116fc6
VladNkov <vladnkov88@gmail.com>
2025-05-15 16:24:07 +0200

    added new task 4.4.12

--- 4. Working with files/4/12/12.py
+++ 4. Working with files/4/12/12.py
@@ -1,2 +1,13 @@
 import json, csv
 
+with open('students.json', encoding='utf-8') as in_file:
+    data = json.load(in_file)
+    sorted_data = sorted(data, key=lambda x: x['name'])
+
+with open('data.csv', 'w', encoding='utf-8', newline='') as out_file:
+    writer = csv.writer(out_file, delimiter=',')
+    writer.writerow(['name', 'phone'])
+    for d in sorted_data:
+        if int(d['age']) >= 18 and int(d['progress']) >= 75:
+            writer.writerow([d['name'], d['phone']])
+
