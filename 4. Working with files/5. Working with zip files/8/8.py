24a4ab9664a4552896c7a628c8b065877f8fae5d
VladNkov <vladnkov88@gmail.com>
2025-05-21 08:25:43 +0200

    added new task 4.5.8

--- 4. Working with files/5. Working with zip files/8/8.py
+++ 4. Working with files/5. Working with zip files/8/8.py
@@ -1,2 +1,13 @@
 from zipfile import ZipFile
 
+
+def extract_this(zip_name, *args):
+    with ZipFile(zip_name, 'r') as zip_file:
+        if len(args) == 0:
+            zip_file.extractall()
+        else:
+            for i in args:
+                zip_file.extract(i)
+
+
+extract_this('workbook.zip', 'earth.jpg', 'exam.txt')
