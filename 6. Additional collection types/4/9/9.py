8306fb2d6e55ed936883d0f2af654625a0e21a00
VladNkov <vladnkov88@gmail.com>
2025-05-30 19:10:45 +0200

    added task 6.4.9

---
+++ 6. Additional collection types/4/9/9.py
@@ -0,0 +1,14 @@
+import pickle
+from collections import namedtuple
+
+Animal = namedtuple('Animal', ['name', 'family', 'sex', 'color'])
+
+with open('data.pkl', 'rb') as file:
+    data = pickle.load(file)
+
+for animal in data:
+    print(f'name: {animal.name}')
+    print(f'family: {animal.family}')
+    print(f'sex: {animal.sex}')
+    print(f'color: {animal.color}')
+    print()
