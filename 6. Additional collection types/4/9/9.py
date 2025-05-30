84a8add1aa8e3fc61fe762c4d0b0ab2e5db82eb7
VladNkov <vladnkov88@gmail.com>
2025-05-30 19:14:05 +0200

    added new in task 6.4.9

--- 6. Additional collection types/4/9/9.py
+++ 6. Additional collection types/4/9/9.py
@@ -7,8 +7,11 @@ with open('data.pkl', 'rb') as file:
     data = pickle.load(file)
 
 for animal in data:
-    print(f'name: {animal.name}')
-    print(f'family: {animal.family}')
-    print(f'sex: {animal.sex}')
-    print(f'color: {animal.color}')
+    # print(f'name: {animal.name}')
+    # print(f'family: {animal.family}')
+    # print(f'sex: {animal.sex}')
+    # print(f'color: {animal.color}')
+    # print()
+    for field, value in zip(Animal._fields, animal):
+        print(f'{field}: {value}')
     print()
