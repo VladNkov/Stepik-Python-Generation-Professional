fcb922fbcd82719a9e9c7e70cd09bcd0135e7d1e
VladNkov <vladnkov88@gmail.com>
2025-06-10 16:22:41 +0200

    added new task 6.7.14

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-from collections import OrderedDict
+from collections import Counter
 
-grades = OrderedDict(Timur=100, Arthur=84, Anri=94, Dima=98)
-new_grades = OrderedDict()
+letters = Counter(('Beautiful is better than ugly'))
 
-for rule in (True, False, False, True):
-    name, grade = grades.popitem(last=rule)
-    new_grades[name] = grade
+print(letters['is'])
 
-print(*new_grades)
